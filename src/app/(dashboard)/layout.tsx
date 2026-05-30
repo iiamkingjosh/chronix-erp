7526b203d1107529e32af6e1bc8bc0ef1c3dfbaa
iiamkingjosh <joshua4moses@gmail.com>
2026-05-30 09:30:07 +0100

    fix(mobile): move PushPrompt inside content wrapper div

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -41,9 +41,8 @@ export default function DashboardLayout({
           <main className="flex-1 overflow-auto pt-14 lg:pt-0">
             {children}
           </main>
+          <PushPrompt />
         </div>
-
-        <PushPrompt />
       </div>
     </ProtectedRoute>
   );
